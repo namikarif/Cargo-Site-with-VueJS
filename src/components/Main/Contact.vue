56ed6afcb27530ebf2650d0f5ce857f5049df8ca
Namig <45629136+namikarif@users.noreply.github.com>
2021-10-16 23:27:07 +0300

    Update Contact.vue

--- src/components/Main/Contact.vue
+++ src/components/Main/Contact.vue
@@ -9,16 +9,16 @@
               <h3>Ünvan</h3>
               <div class="address-block">
                 <ul>
-                  <li class="address-icon"><strong>Ünvan:</strong><br>Fətəli Xan Xoyski 111A, Bakı</li>
-                  <li class="phone-icon"><strong>Telefon</strong><br>+994(12) 564 5190</li>
-                  <li class="mobile-icon"><strong>Mobil</strong><br> +994(51) 564 5190</li>
-                  <li class="email-icon"><strong>Email:</strong><br><a href="#" title="">info@cargodiem.az</a></li>
+                  <li class="address-icon"><strong>Ünvan:</strong><br>namikarifoglu.com</li>
+                  <li class="phone-icon"><strong>Telefon</strong><br>+994</li>
+                  <li class="mobile-icon"><strong>Mobil</strong><br> +994</li>
+                  <li class="email-icon"><strong>Email:</strong><br><a href="#" title="">info@namikarifoglu.com</a></li>
                 </ul>
               </div>
             </div>
           </div>
           <div class="col-12 col-lg-8 col-md-8 col-sm-8">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.1895008923384!2d49.864436015171655!3d40.404652564298274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307d646c5a1eb3%3A0x33c89d634cd0c00a!2sCargoDiem!5e0!3m2!1sen!2sua!4v1604838457613!5m2!1sen!2sua" class="col-lg-12 col-md-12" height="270" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.1895008923384!2d42.864436015171655!3d45.404652564298274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307d646c5a1eb3%3A0x33c89d634cd0c00a!2sCargoDiem!5e0!3m2!1sen!2sua!4v1604838457613!5m2!1sen!2sua" class="col-lg-12 col-md-12" height="270" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
           </div>
         </div>
       </div>
