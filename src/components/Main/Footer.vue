c99b61cd8eac16bf9e6889e19e0e364c9ec8d3bb
Namig <45629136+namikarif@users.noreply.github.com>
2021-10-16 23:25:59 +0300

    Update Footer.vue

--- src/components/Main/Footer.vue
+++ src/components/Main/Footer.vue
@@ -6,15 +6,15 @@
 
           <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
             <div class="widget">
-              <a class="phone-call" href="tel:+994513164501"><i class="fas fa-phone" style="margin-right:7px"></i>+994(51) 564 5190  </a>
-              <p style="color:white">Ünvan: Fətəli Xan Xoyski 111A</p>
-              <p style="color: white;">© 2020 | CargoDiem | Gözünüz yolda qalmayacaq!</p>
+              <a class="phone-call" href="tel:+994"><i class="fas fa-phone" style="margin-right:7px"></i>+994  </a>
+              <p style="color:white">namikarifoglu.com</p>
+              <p style="color: white;">© 2020 | Cargo</p>
               <p style="color:white">Qrafik (iş saatlarımız): B.e - Ş. 10:00 - 19:00</p>
             </div>
             <div class="footer__b">
-              <a href="https://www.facebook.com/cargodiem.baku" target="”_blank”"><i class="fab fa-facebook-f"></i></a>
-              <a href="https://www.instagram.com/cargo.diem " target="”_blank”"><i class="fab fa-instagram"></i></a>
-              <a href="https://www.linkedin.com/company/cargodiem" target="”_blank”"><i class="fab fa-linkedin-in"></i></a>
+              <a href="https://www.facebook.com/namikarifoglu" target="”_blank”"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://www.instagram.com/namikarifoglu" target="”_blank”"><i class="fab fa-instagram"></i></a>
+              <a href="https://www.linkedin.com/in/namikarifoglu" target="”_blank”"><i class="fab fa-linkedin-in"></i></a>
 
             </div>
           </div>
@@ -37,7 +37,7 @@
 
             <div class="col-lg-4 d-none d-lg-block">
               <div class="widget">
-                <div class="widget-title">CARGODIEM </div>
+                <div class="widget-title">namikarifoglu.com </div>
                 <ul class="list">
                   <!--<li><a href="/tariflerimiz">Tariflər</a></li>-->
                   <li> <a class=" " href="/shops">Mağazalar</a></li>
@@ -65,7 +65,7 @@
 
               <div class="col-md-6 w-50">
                 <div class="widget">
-                  <div class="widget-title">CARGODIEM </div>
+                  <div class="widget-title">namikarifoglu.com </div>
                   <ul class="list">
                     <!--<li><a href="/tariflerimiz">Tariflər</a></li>-->
                     <li> <a class=" " href="/shops">Mağazalar</a></li>
